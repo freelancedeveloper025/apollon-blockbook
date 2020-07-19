7b85894cdc01f1d2395e6e469454244775b4055e
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:03:31 +0200

    Update txcache.go

--- db/txcache.go
+++ db/txcache.go
@@ -3,9 +3,9 @@ package db
 import (
 	"github.com/golang/glog"
 	"github.com/juju/errors"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/bchain/coins/eth"
-	"github.com/trezor/blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/bchain/coins/eth"
+	"github.com/stepollo2/apollon-blockbook/common"
 )
 
 // TxCache is handle to TxCacheServer
