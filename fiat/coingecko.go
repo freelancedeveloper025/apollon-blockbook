1698dcf8a4374802f1f4e42d82d22dad0915c45d
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:04:11 +0200

    Update coingecko.go

--- fiat/coingecko.go
+++ fiat/coingecko.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/golang/glog"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 // Coingecko is a structure that implements RatesDownloaderInterface
