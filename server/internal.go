b378ca86372fcfda2cf56b2e0b39c496f7581a27
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:06:38 +0200

    Update internal.go

--- server/internal.go
+++ server/internal.go
@@ -8,10 +8,10 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/trezor/blockbook/api"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/api"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 // InternalServer is handle to internal http server
