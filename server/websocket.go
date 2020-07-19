d321f9fe93ffedfb877de8ec7f3442f9e7c63b35
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:09:47 +0200

    Update websocket.go

--- server/websocket.go
+++ server/websocket.go
@@ -14,10 +14,10 @@ import (
 	"github.com/golang/glog"
 	"github.com/gorilla/websocket"
 	"github.com/juju/errors"
-	"github.com/trezor/blockbook/api"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/api"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 const upgradeFailed = "Upgrade failed: "
