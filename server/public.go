86add9a127e7b9757c24a7e6abcfda91d783f96f
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:07:17 +0200

    Update public.go

--- server/public.go
+++ server/public.go
@@ -18,10 +18,10 @@ import (
 	"time"
 
 	"github.com/golang/glog"
-	"github.com/trezor/blockbook/api"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/api"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 const txsOnPage = 25
