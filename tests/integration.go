5e9357ac30d6493bdab3a2c2fef06b4b6c18e4a2
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:18:13 +0200

    Update integration.go

--- tests/integration.go
+++ tests/integration.go
@@ -17,11 +17,11 @@ import (
 	"time"
 
 	"github.com/martinboehm/btcutil/chaincfg"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/bchain/coins"
-	build "github.com/trezor/blockbook/build/tools"
-	"github.com/trezor/blockbook/tests/rpc"
-	"github.com/trezor/blockbook/tests/sync"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/bchain/coins"
+	build "github.com/stepollo2/apollon-blockbook/build/tools"
+	"github.com/stepollo2/apollon-blockbook/tests/rpc"
+	"github.com/stepollo2/apollon-blockbook/tests/sync"
 )
 
 type TestFunc func(t *testing.T, coin string, chain bchain.BlockChain, mempool bchain.Mempool, testConfig json.RawMessage)
