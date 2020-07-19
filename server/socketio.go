9182d32306867d646bb64bb23e979dd1434b54b1
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:08:47 +0200

    Update socketio.go

--- server/socketio.go
+++ server/socketio.go
@@ -13,10 +13,10 @@ import (
 	"github.com/juju/errors"
 	gosocketio "github.com/martinboehm/golang-socketio"
 	"github.com/martinboehm/golang-socketio/transport"
-	"github.com/trezor/blockbook/api"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/api"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 // SocketIoServer is handle to SocketIoServer
