9ee3b294135baba95a7efd404f06ad1fc8a07b9b
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:40:28 +0200

    edit reminder

--- bchain/coins/xap/xapparser.go
+++ bchain/coins/xap/xapparser.go
@@ -1,1 +1,52 @@
+package polis
 
+import (
+	"github.com/grupokindynos/coins-explorer/bchain"
+	"github.com/grupokindynos/coins-explorer/bchain/coins/btc"
+
+	"github.com/martinboehm/btcd/wire"
+	"github.com/martinboehm/btcutil/chaincfg"
+)
+
+const (
+	MainnetMagic wire.BitcoinNet = 0xbd6b0cbf
+)
+
+var (
+	MainNetParams chaincfg.Params
+)
+
+func init() {
+	MainNetParams = chaincfg.MainNetParams
+	MainNetParams.Net = MainnetMagic
+
+	MainNetParams.PubKeyHashAddrID = []byte{55}
+	MainNetParams.ScriptHashAddrID = []byte{56}
+}
+
+type PolisParser struct {
+	*btc.BitcoinParser
+	baseparser *bchain.BaseParser
+}
+
+func NewPolisParser(params *chaincfg.Params, c *btc.Configuration) *PolisParser {
+	return &PolisParser{BitcoinParser: btc.NewBitcoinParser(params, c), baseparser: &bchain.BaseParser{}}
+}
+
+func GetChainParams(chain string) *chaincfg.Params {
+	if !chaincfg.IsRegistered(&MainNetParams) {
+		err := chaincfg.Register(&MainNetParams)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return &MainNetParams
+}
+
+func (p *PolisParser) PackTx(tx *bchain.Tx, height uint32, blockTime int64) ([]byte, error) {
+	return p.baseparser.PackTx(tx, height, blockTime)
+}
+
+func (p *PolisParser) UnpackTx(buf []byte) (*bchain.Tx, uint32, error) {
+	return p.baseparser.UnpackTx(buf)
+}
