2ba28670ea99b4f7622da46ab083c1823e04a6a7
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:24:03 +0200

    Update sync.go

--- tests/sync/sync.go
+++ tests/sync/sync.go
@@ -10,9 +10,9 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 var testMap = map[string]func(t *testing.T, th *TestHandler){
