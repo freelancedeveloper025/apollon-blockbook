1c99fed208514ff4cbb528c2845b59cc48593f18
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:00:03 +0200

    Update rocksdb.go

--- db/rocksdb.go
+++ db/rocksdb.go
@@ -18,8 +18,8 @@ import (
 	"github.com/golang/glog"
 	"github.com/juju/errors"
 	"github.com/tecbot/gorocksdb"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
 )
 
 const dbVersion = 5
