7ef5f2953dba6f58e9e1cea12653e21252cec5ab
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:05:33 +0200

    Update fiat_rates_test.go

--- fiat/fiat_rates_test.go
+++ fiat/fiat_rates_test.go
@@ -14,10 +14,10 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/martinboehm/btcutil/chaincfg"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/bchain/coins/btc"
-	"github.com/trezor/blockbook/common"
-	"github.com/trezor/blockbook/db"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/bchain/coins/btc"
+	"github.com/stepollo2/apollon-blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/db"
 )
 
 func TestMain(m *testing.M) {
