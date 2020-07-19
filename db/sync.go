2f3fc43742cfba098bb0d628cd373144dee7004b
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-19 19:02:32 +0200

    Update sync.go

--- db/sync.go
+++ db/sync.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/juju/errors"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
 )
 
 // SyncWorker is handle to SyncWorker
