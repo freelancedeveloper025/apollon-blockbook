a658fab2b20081a976b9ff78671ba8b01edd9097
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-22 19:59:24 +0200

    Update digibyteparser.go

--- bchain/coins/digibyte/digibyteparser.go
+++ bchain/coins/digibyte/digibyteparser.go
@@ -3,7 +3,7 @@ package digibyte
 import (
 	"github.com/martinboehm/btcd/wire"
 	"github.com/martinboehm/btcutil/chaincfg"
-	"github.com/trezor/blockbook/bchain/coins/btc"
+	"github.com/stepollo2/apollon-blockbook/bchain/coins/btc"
 )
 
 const (
