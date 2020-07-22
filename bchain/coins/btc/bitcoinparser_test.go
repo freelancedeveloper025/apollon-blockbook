a9df63e8cfae5eae9a175589c265773a8aa554fa
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-22 16:43:26 +0200

    Update bitcoinparser_test.go

--- bchain/coins/btc/bitcoinparser_test.go
+++ bchain/coins/btc/bitcoinparser_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/martinboehm/btcutil/chaincfg"
-	"github.com/trezor/blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/bchain"
 )
 
 func TestMain(m *testing.M) {
