6d39f4b9572bb2f3eab8c210eca4829d2efc0a00
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-22 16:43:51 +0200

    Update bitcoinrpc.go

--- bchain/coins/btc/bitcoinrpc.go
+++ bchain/coins/btc/bitcoinrpc.go
@@ -16,8 +16,8 @@ import (
 	"github.com/golang/glog"
 	"github.com/juju/errors"
 	"github.com/martinboehm/btcd/wire"
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/common"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/common"
 )
 
 // BitcoinRPC is an interface to JSON-RPC bitcoind service.
