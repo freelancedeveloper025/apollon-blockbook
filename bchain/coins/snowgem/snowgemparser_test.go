0cc10357c444baf4a2f90cf67cfd52cfc1c6f73e
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-07-23 14:47:11 +0200

    Update snowgemparser_test.go

--- bchain/coins/snowgem/snowgemparser_test.go
+++ bchain/coins/snowgem/snowgemparser_test.go
@@ -10,8 +10,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/trezor/blockbook/bchain"
-	"github.com/trezor/blockbook/bchain/coins/btc"
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/bchain/coins/btc"
 
 	"github.com/martinboehm/btcutil/chaincfg"
 )
