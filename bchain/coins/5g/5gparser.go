bfa1c36a265d6b27f8960722a032fab95ec1ba15
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-12-16 18:05:45 +0100

    Create 5gparser.go

---
+++ bchain/coins/5g/5gparser.go
@@ -0,0 +1,52 @@
+package 5g
+
+import (
+	"github.com/stepollo2/apollon-blockbook/bchain"
+	"github.com/stepollo2/apollon-blockbook/bchain/coins/btc"
+
+	"github.com/martinboehm/btcd/wire"
+	"github.com/martinboehm/btcutil/chaincfg"
+)
+
+const (
+	MainnetMagic wire.BitcoinNet = 0xe92caf4c
+)
+
+var (
+	MainNetParams chaincfg.Params
+)
+
+func init() {
+	MainNetParams = chaincfg.MainNetParams
+	MainNetParams.Net = MainnetMagic
+
+	MainNetParams.PubKeyHashAddrID = []byte{48}
+	MainNetParams.ScriptHashAddrID = []byte{13}
+}
+
+type 5gParser struct {
+	*btc.BitcoinParser
+	baseparser *bchain.BaseParser
+}
+
+func New5gParser(params *chaincfg.Params, c *btc.Configuration) *5gParser {
+	return &5gParser{BitcoinParser: btc.NewBitcoinParser(params, c), baseparser: &bchain.BaseParser{}}
+}
+
+func GetChainParams(chain string) *chaincfg.Params {
+	if !chaincfg.IsRegistered(&MainNetParams) {
+		err := chaincfg.Register(&MainNetParams)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return &MainNetParams
+}
+
+func (p *5gParser) PackTx(tx *bchain.Tx, height uint32, blockTime int64) ([]byte, error) {
+	return p.baseparser.PackTx(tx, height, blockTime)
+}
+
+func (p *5gParser) UnpackTx(buf []byte) (*bchain.Tx, uint32, error) {
+	return p.baseparser.UnpackTx(buf)
+}
